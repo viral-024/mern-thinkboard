002f2c48c9ebbd4a7ff402967b0ba55244d2b115
viral-024 <virudafda007@gmail.com>
2026-06-17 12:17:48 +0530

    prepared for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
 
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDb } from "./config/db.js";
@@ -10,34 +11,34 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 // Middleware
-app.use(cors({
-    origin: "http://localhost:5173", // Adjust this to your frontend URL
-}));
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173", // Adjust this to your frontend URL
+    }),
+  );
+}
 app.use(express.json());
 app.use(rateLimiter);
-
-// Custom Logger Middleware
-/*
-app.use((req, res, next) => {
-    console.log(`Request Method: ${req.method}`);
-    console.log(`Request URL: ${req.url}`);
-    next();
-});
-*/
 
 // Routes
 app.use("/api/notes", notesRoutes);
 
-// Root Route
-app.get("/", (req, res) => {
-    res.send("API is running...");
-});
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-connectDb().then(() =>{
-    // Start Server
-    app.listen(PORT, () => {
-        console.log(`Server is running on port: ${PORT}`);
-    });
-})
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
+
+// Connect to MongoDB and start the server
+connectDb().then(() => {
+  // Start Server
+  app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
+  });
+});
